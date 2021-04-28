eb91ec087dd2c5aa23fbde4cc28f3880d13d03ee
Sopee Thong <tsopee@gmail.com>
2021-04-28 01:54:30 -0400

    adding post to create post

--- controllers/commentRoutes.js
+++ controllers/commentRoutes.js
@@ -74,3 +74,15 @@ router.get('/', (req, res) => {
         res.status(500).json(err);
       });
   });
+  router.post('/', withAuth, (req, res) => {
+    Post.create({
+      title: req.body.title,
+      post_content: req.body.post_content,
+      user_id: req.session.user_id
+    })
+      .then(dbPostData => res.json(dbPostData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+});

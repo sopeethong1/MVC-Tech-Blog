77564c3d9d3f4963823e7edddebace33eac781ce
Sopee Thong <tsopee@gmail.com>
2021-04-28 01:46:38 -0400

    updated with commentRoutes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,11 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const blogRoutes = require('./blogRoutes');
+const commentRoutes = require('./commentRoutes');
+
 
 router.use('/users', userRoutes);
 router.use('/blogs', blogRoutes);
+route.use('/comments', commentRoutes);
 
 module.exports = router;

588dd8e4871bc9409c0763c0341a41e1c83931df
Sopee Thong <tsopee@gmail.com>
2021-04-29 21:29:36 -0400

    adding logout response

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,6 +1,14 @@
 const logout = async () => {
-    // Make a POST request to destroy the session on the back end
-    const response = await fetch('/api/users/logout', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-    });
+  const response = await fetch('/api/users/logout', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+  });
+
+  if (response.ok) {
+    document.location.replace('/');
+  } else {
+    alert(response.statusText);
+  }
+};
+
+document.querySelector('#logout').addEventListener('click', logout);

a44ee0bb73577d9d1ecc8ba1b41998528b0dedce
Sopee Thong <tsopee@gmail.com>
2021-04-29 21:27:05 -0400

    reformatting

--- public/js/login.js
+++ public/js/login.js
@@ -1,19 +1,3 @@
 const loginFormHandler = async (event) => {
     // Stop the browser from submitting the form so we can do so with JavaScript
-    async function loginFormHandler(event) {
-        event.preventDefault();
-      
-        const email = document.querySelector('#email-login').value.trim();
-        const password = document.querySelector('#password-login').value.trim();
-    
-      
-          if (response.ok) {
-            document.location.replace('/dashboard');
-          } else {
-            alert(response.statusText);
-          }
-        }
-      }
-    
-      document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
     event.preventDefault();
